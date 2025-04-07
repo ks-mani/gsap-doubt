eb60e5cd0a5c8c1a09d9aab970f028d3e22e6d3b
Manish <kumar.mani016@gmail.com>
2025-04-08 01:27:24 +0530

    Anim

--- src/Component2/index.js
+++ src/Component2/index.js
@@ -2,14 +2,12 @@ import "./index.css";
 
 const Component2 = () => {
   return (
-    <div
-      className="widget component2Wrap"
-      style={{
-        border: "2px solid red",
-      }}
-    >
-      <p>Component2</p>
+    <div className="widget component2Wrap">
+      <p>
+        Component2 : Has Animation.. And GSAP logic get reexecuted on rerender
+      </p>
       <div className="comp2pin">
+        <p>Pinned Container</p>
         <div className="comp2Anim"></div>
       </div>
     </div>
--- src/Component4/index.js
+++ src/Component4/index.js
@@ -1,5 +1,17 @@
+import "./index.css";
+
 const Component4 = () => {
-  return <div className="widget">Component4</div>;
+  return (
+    <div className="widget component4Wrap">
+      <p>
+        Component4 : Has Animation.. And GSAP logic get executed on mount only
+      </p>
+      <div className="comp4pin">
+        <p>Pinned Container</p>
+        <div className="comp4Anim"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Component4;
